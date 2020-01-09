cbb03077a17b09d827c6f3df48e65bedd3c443dc
Arabian Mickael <mickaelarabian@gmail.com>
2020-01-09 17:24:03 +0100

    start Auth code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,19 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function displayLogin(){
-        return view('login');
+        if(Auth::check()){
+            return redirect('/');
+        } else{
+            return view('login');
+        }
     }
 
     public function login(Request $request){
-        auth()->attempt([
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
-        ]);
-        return redirect('/');
+        if(!Auth::check()){
+           $mail = $request->get('email');
+           $password = $request->get('password');
+           
+            return redirect('/');
+        } else{
+            return redirect('/');
+        }
+
+
     }
 
     public function logout(){
